135ae6b103e2df2825610e3cf020eae18d2d142c
sagar <sagar.kerinfotech@gmail.com>
2022-11-28 15:00:27 +0530

    setup : basic folder setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import "./index.css";
+import { Cart } from "./tools/svgs";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Cart className="svgsize" />
+    </div>
+  );
 }
 
 export default App;
